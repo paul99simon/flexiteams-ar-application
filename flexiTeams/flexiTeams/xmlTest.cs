4f23888c874233b4fa39f60659fcd9023179e3db
Paul Simon <s4plsimo@uni-trier.de>
2023-04-23 23:41:08 +0200

    Changes: - resource_pool_draft.xml added - resource_pool.xsd added

---
+++ flexiTeams/flexiTeams/xmlTest.cs
@@ -0,0 +1,13 @@
+﻿namespace flexiTeams;
+
+using System.Xml;
+class XmlTest
+{
+    static void Main()
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.Load("..\\..\\..\\..\\..\\resourcePools\\resource_pool_draft.xml");
+        XmlNode node = doc.DocumentElement.SelectSingleNode("/resourcePool/resource/name/firstName[@v='Erika']/../..");
+        Console.WriteLine(node.Attributes.GetNamedItem("age").Value);
+    }
+}
