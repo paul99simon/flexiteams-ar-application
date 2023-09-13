4dba383d511e271b4954a80509d3dfc46ae8b13a
Paul Simon <s4plsimo@uni-trier.de>
2023-09-13 22:22:28 +0200

    SavePoint

--- unityProjectVR/Assets/Workflow3DManager.cs
+++ unityProjectVR/Assets/Workflow3DManager.cs
@@ -61,7 +61,27 @@ public class Workflow3DManager : MonoBehaviour
     void Start()
     {
         BasicGraphDirector.ConstructFromCsv(path, _graph, new BasicWorkflowBuilder(), new BasicTaskBuilder());
-        Create3DWorkflowLayout();
+        //Create3DWorkflowLayout();
+        var cornerObject = new GameObject("Corner");
+        var meshFilter1 = cornerObject.AddComponent<MeshFilter>();
+        var meshRenderer1 = cornerObject.AddComponent<MeshRenderer>();
+
+        var tubeObject = new GameObject("Tube");
+        var meshFilter2 = tubeObject.AddComponent<MeshFilter>();
+        var meshRenderer2 = tubeObject.AddComponent<MeshRenderer>();
+
+        var uObject = new GameObject("U-Profil");
+        var meshFilter3 = uObject.AddComponent<MeshFilter>();
+        var meshRenderer3 = uObject.AddComponent<MeshRenderer>();
+
+        var planeObject = new GameObject("Planes");
+        var meshFilter4 = planeObject.AddComponent<MeshFilter>();
+        var meshRenderer4 = planeObject.AddComponent<MeshRenderer>();
+
+        meshFilter1.mesh = CreateCornerMesh(0.05f);
+        meshFilter2.mesh = CreateTubeMesh(new Vector2(0.05f, 1));
+        meshFilter3.mesh = CreateUMesh(0.05f);
+        meshFilter4.mesh = CreateParallelSquarePlaneMesh(0.05f);
     }
 
     private void Create3DWorkflowLayout()
@@ -156,6 +176,8 @@ public class Workflow3DManager : MonoBehaviour
 
         return cuboidObject;
     }
+
+    //Mesh generation
     private Mesh CreateCuboidMesh(Vector3 dimensions)
     {
         var mesh = new Mesh()
@@ -264,6 +286,286 @@ public class Workflow3DManager : MonoBehaviour
 
         return mesh;
     }
+    private Mesh CreateCornerMesh(float dimension)
+    {
+        var mesh = new Mesh()
+        {
+            name = "CornerMesh"
+        };
+
+        float deltaWidth    = dimension / 2;
+        float deltaHeight   = dimension / 2;
+        float deltaDepth    = dimension / 2;
+
+        //Back
+        var E = new Vector3(-deltaWidth, -deltaHeight, deltaDepth);
+        var F = new Vector3(deltaWidth, -deltaHeight, deltaDepth);
+        var G = new Vector3(deltaWidth, deltaHeight, deltaDepth);
+        var H = new Vector3(-deltaWidth, deltaHeight, deltaDepth);
+        //Top
+        var I = new Vector3(-deltaWidth, deltaHeight, -deltaDepth);
+        var J = new Vector3(deltaWidth, deltaHeight, -deltaDepth);
+        var K = G;
+        var L = H;
+        //Left
+        var Q = E;
+        var R = new Vector3(-deltaWidth, -deltaHeight, -deltaDepth);
+        var S = I;
+        var T = H;
+        //Rigth
+        var U = new Vector3(deltaWidth, -deltaHeight, -deltaDepth);
+        var V = F;
+        var W = G;
+        var X = J;
+
+        int e = 0;
+        int f = 1;
+        int g = 2;
+        int h = 3;
+        int i = 4;
+        int j = 5;
+        int k = 6;
+        int l = 7;
+        int q = 8;
+        int r = 9;
+        int s = 10;
+        int t = 11;
+        int u = 12;
+        int v = 13;
+        int w = 14;
+        int x = 15;
+
+        mesh.vertices = new Vector3[] { E, F, G, H, I, J, K, L, Q, R, S, T, U, V, W, X };
+        mesh.triangles = new int[]
+        {
+                //Back Face
+                f, g, e,
+                g, h, e,
+                //TopFace
+                i, l, j,
+                l, k, j,
+                //LeftFace
+                q, t, r,
+                t, s, r,
+                //RightFace
+                u, x, v,
+                x, w, v
+        };
+        mesh.normals = new Vector3[]
+        {
+                //Back
+                Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+                //Top
+                Vector3.up, Vector3.up, Vector3.up, Vector3.up,
+                //Left
+                Vector3.left, Vector3.left, Vector3.left, Vector3.left,
+                //Rigth
+                Vector3.right, Vector3.right, Vector3.right, Vector3.right
+        };
+
+        return mesh;
+    }
+    private Mesh CreateTubeMesh(Vector2 dimensions)
+    {
+        var mesh = new Mesh()
+        {
+            name = "TubeMesh"
+        };
+
+        float deltaWidth    = dimensions.x / 2;
+        float deltaHeight   = dimensions.y / 2;
+        float deltaDepth    = dimensions.x / 2;
+
+        //Front
+        var A = new Vector3(-deltaWidth, -deltaHeight, -deltaDepth);
+        var B = new Vector3(deltaWidth, -deltaHeight, -deltaDepth);
+        var C = new Vector3(deltaWidth, deltaHeight, -deltaDepth);
+        var D = new Vector3(-deltaWidth, deltaHeight, -deltaDepth);
+        //Back
+        var E = new Vector3(-deltaWidth, -deltaHeight, deltaDepth);
+        var F = new Vector3(deltaWidth, -deltaHeight, deltaDepth);
+        var G = new Vector3(deltaWidth, deltaHeight, deltaDepth);
+        var H = new Vector3(-deltaWidth, deltaHeight, deltaDepth);
+
+        //Left
+        var Q = E;
+        var R = A;
+        var S = D;
+        var T = H;
+        //Rigth
+        var U = B;
+        var V = F;
+        var W = G;
+        var X = C;
+
+        var a = 0;
+        var b = 1;
+        var c = 2;
+        var d = 3;
+        var e = 4;
+        var f = 5;
+        var g = 6;
+        var h = 7;
+
+        var q = 8;
+        var r = 9;
+        var s = 10;
+        var t = 11;
+        var u = 12;
+        var v = 13;
+        var w = 14;
+        var x = 15;
+
+
+        mesh.vertices = new Vector3[] { A, B, C, D, E, F, G, H, Q, R, S, T, U, V, W, X };
+        mesh.triangles = new int[]
+        {
+                //Front Face
+                a, d, b,
+                d, c, b,
+                //Back Face
+                f, g, e,
+                g, h, e,
+                //LeftFace
+                q, t, r,
+                t, s, r,
+                //RightFace
+                u, x, v,
+                x, w, v
+        };
+        mesh.normals = new Vector3[]
+        {
+                //Front
+                Vector3.back, Vector3.back, Vector3.back, Vector3.back,
+                //Back
+                Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+                //Left
+                Vector3.left, Vector3.left, Vector3.left, Vector3.left,
+                //Rigth
+                Vector3.right, Vector3.right, Vector3.right, Vector3.right
+        };
+
+        return mesh;
+    }
+    private Mesh CreateUMesh(float dimension)
+    {
+        var mesh = new Mesh()
+        {
+            name = "UMesh"
+        };
+
+        float deltaWidth    = dimension / 2;
+        float deltaHeight   = dimension / 2;
+        float deltaDepth    = dimension / 2;
+
+        //Back
+        var E = new Vector3(-deltaWidth, -deltaHeight, deltaDepth);
+        var F = new Vector3(deltaWidth, -deltaHeight, deltaDepth);
+        var G = new Vector3(deltaWidth, deltaHeight, deltaDepth);
+        var H = new Vector3(-deltaWidth, deltaHeight, deltaDepth);
+
+        //Left
+        var Q = E;
+        var R = new Vector3(-deltaWidth, -deltaHeight, -deltaDepth); ;
+        var S = new Vector3(-deltaWidth, deltaHeight, -deltaDepth); ;
+        var T = H;
+        //Rigth
+        var U = new Vector3(deltaWidth, -deltaHeight, -deltaDepth); ;
+        var V = F;
+        var W = G;
+        var X = new Vector3(deltaWidth, deltaHeight, -deltaDepth); ;
+
+        var e = 0;
+        var f = 1;
+        var g = 2;
+        var h = 3;
+        var q = 4;
+        var r = 5;
+        var s = 6;
+        var t = 7;
+        var u = 8;
+        var v = 9;
+        var w = 10;
+        var x = 11;
+
+
+        mesh.vertices = new Vector3[] { E, F, G, H, Q, R, S, T, U, V, W, X };
+        mesh.triangles = new int[]
+        {
+                //Back Face
+                f, g, e,
+                g, h, e,
+                //LeftFace
+                q, t, r,
+                t, s, r,
+                //RightFace
+                u, x, v,
+                x, w, v
+        };
+        mesh.normals = new Vector3[]
+        {
+                //Back
+                Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+                //Left
+                Vector3.left, Vector3.left, Vector3.left, Vector3.left,
+                //Rigth
+                Vector3.right, Vector3.right, Vector3.right, Vector3.right
+        };
+
+        return mesh;
+    }
+    private Mesh CreateParallelSquarePlaneMesh(float dimension)
+    {
+        var mesh = new Mesh()
+        {
+            name = "ParallelPlaneMesh"
+        };
+
+        float deltaWidth    = dimension / 2;
+        float deltaHeight   = dimension / 2;
+        float deltaDepth    = dimension / 2;
+
+        //Front
+        var A = new Vector3(-deltaWidth, -deltaHeight, -deltaDepth);
+        var B = new Vector3(deltaWidth, -deltaHeight, -deltaDepth);
+        var C = new Vector3(deltaWidth, deltaHeight, -deltaDepth);
+        var D = new Vector3(-deltaWidth, deltaHeight, -deltaDepth);
+        //Back
+        var E = new Vector3(-deltaWidth, -deltaHeight, deltaDepth);
+        var F = new Vector3(deltaWidth, -deltaHeight, deltaDepth);
+        var G = new Vector3(deltaWidth, deltaHeight, deltaDepth);
+        var H = new Vector3(-deltaWidth, deltaHeight, deltaDepth);
+        
+        var a = 0;
+        var b = 1;
+        var c = 2;
+        var d = 3;
+        var e = 4;
+        var f = 5;
+        var g = 6;
+        var h = 7;
+
+        mesh.vertices = new Vector3[] { A, B, C, D, E, F, G, H };
+        mesh.triangles = new int[]
+        {
+                //Front Face
+                a, d, b,
+                d, c, b,
+                //Back Face
+                f, g, e,
+                g, h, e,
+        };
+        mesh.normals = new Vector3[]
+        {
+                //Front
+                Vector3.back, Vector3.back, Vector3.back, Vector3.back,
+                //Back
+                Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+        };
+
+        return mesh;
+    }
+
     private GameObject CreateTextObject(string text)
     {
         //GameObject
